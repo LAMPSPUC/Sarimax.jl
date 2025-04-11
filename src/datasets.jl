36a5ac22e24f1a26fc76a48b6e563140bb7a7973
LuizFCDuarte <luizduarte@aluno.puc-rio.com.br>
2025-04-11 13:00:34 -0300

    :sparkles: Improve datasets docs

--- src/datasets.jl
+++ src/datasets.jl
@@ -1,8 +1,32 @@
+"""
+The Datasets Enum is used to identify the dataset used in the `loadDataset` function.
+
+The Datasets Enum is defined as follows:
+
+- `AIR_PASSENGERS = 1`
+- `GDPC1 = 2`
+- `NROU = 3`
+
+The `loadDataset` function uses this Enum to determine the dataset to be loaded.
+"""
 @enum Datasets begin
     AIR_PASSENGERS = 1
     GDPC1 = 2
     NROU = 3
 end
+
+@doc("
+The AirPassengers dataset, it contains the classic Box-Jenkins airline data.
+", AIR_PASSENGERS)
+
+@doc("
+The GDPC1 dataset, it contains the US Real Gross Domestic Product (GDPC1) from 1947 to 2019.
+", GDPC1)
+
+@doc("
+The NROU dataset, it contains the number of new orders received by US manufacturers from 1960 to 2001.
+", NROU)
+
 
 const AIR_PASSENGERS = instances(Datasets)[1]
 const GDPC1 = instances(Datasets)[2]
@@ -22,7 +46,7 @@ datasetsPaths = [
         dataset::Datasets
     )
 
-Loads a dataset from the `Datasets` enum. 
+Loads a dataset from the `Datasets` enum.
 
 # Example
 ```jldoctest
@@ -40,7 +64,7 @@ julia> airPassengers = loadDataset(AIR_PASSENGERS)
 function loadDataset(dataset::Datasets)
     datasetIndex = Int(dataset)
     seriesData = CSV.read(datasetsPaths[datasetIndex], DataFrame)
-    y = TimeArray(seriesData, timestamp = :date)
+    y = TimeArray(seriesData, timestamp=:date)
     return y
 end
 
@@ -71,14 +95,14 @@ julia> airPassengers = loadDataset(airPassengersDf)
 
 ```
 """
-function loadDataset(df::DataFrame, showLogs::Bool = false)
+function loadDataset(df::DataFrame, showLogs::Bool=false)
     auxiliarDF = deepcopy(df)
     if !(:date in propertynames(auxiliarDF))
         showLogs && @info("The DataFrame does not have a column named 'date'.")
         showLogs && @info("Creating a date column with the index of the DataFrame")
         auxiliarDF[!, :date] = [Date(i) for i = 1:size(auxiliarDF, 1)]
     end
-    y = TimeArray(auxiliarDF, timestamp = :date)
+    y = TimeArray(auxiliarDF, timestamp=:date)
     return y
 end
 
@@ -88,11 +112,11 @@ end
         trainSize::Fl=0.8
     ) where Fl<:AbstractFloat
 
-Splits the time series in training and testing sets. 
+Splits the time series in training and testing sets.
 """
 function splitTrainTest(
     data::TimeArray;
-    trainPercentage::Fl = 0.8,
+    trainPercentage::Fl=0.8,
 ) where {Fl<:AbstractFloat}
     trainingSetEndIndex = floor(Int, trainPercentage * length(data))
     trainingSet = TimeArray(
